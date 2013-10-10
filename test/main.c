216f51273737e1d390b73f2b4ab69f15851471d5
Stephan Gatzka <stephan.gatzka@gmail.com>
2013-10-10 20:43:57 +0200

    Change port to "official" jet port.

--- test/main.c
+++ test/main.c
@@ -12,7 +12,7 @@ int main()
 	memset(&serv_addr, 0, sizeof(serv_addr));
 	serv_addr.sin_family = AF_INET;
 	serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
-	serv_addr.sin_port = htons(7899);
+	serv_addr.sin_port = htons(11122);
 
 	int fd = socket(AF_INET, SOCK_STREAM, 0);
 
