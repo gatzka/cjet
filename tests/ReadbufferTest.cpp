eb2509cb7acebde122e9fa067b3691ae07bf1efd
Stephan Gatzka <stephan.gatzka@gmail.com>
2013-10-13 15:35:04 +0200

    Add more unit tests.
    Now we have tests to check the behaviour of get_read_ptr when the client
    closed the connection, when the client messages where too big and when
    the read call would block.

--- tests/ReadbufferTest.cpp
+++ tests/ReadbufferTest.cpp
@@ -33,7 +33,13 @@ $Date:$
 #include <errno.h>
 #include <stdint.h>
 
+#include "../config.h"
 #include "../peer.h"
+
+static const int BADFD = -1;
+static const int TOO_MUCH_DATA = 1;
+static const int CLIENT_CLOSE = 2;
+static const int AGAIN = 3;
 
 extern "C" {
 
@@ -44,18 +50,24 @@ int parse_message(char *msg, uint32_t length)
 
 ssize_t fake_read(int fd, void *buf, size_t count)
 {
-	if (fd == -1) {
+	if (fd == BADFD) {
 		errno = EBADF;
 		return -1;
 	}
-	return 0;
+	if (fd == CLIENT_CLOSE) {
+		return 0;
+	}
+	if (fd == AGAIN) {
+		errno = EAGAIN;
+		return -1;
+	}
 }
 
 }
 
 BOOST_AUTO_TEST_CASE(wrong_fd)
 {
-	struct peer *p = alloc_peer(-1);
+	struct peer *p = alloc_peer(BADFD);
 	BOOST_REQUIRE(p != NULL);
 
 	char *read_ptr = get_read_ptr(p, 100);
@@ -64,3 +76,35 @@ BOOST_AUTO_TEST_CASE(wrong_fd)
 	free_peer(p);
 }
 
+BOOST_AUTO_TEST_CASE(too_much_data_requested)
+{
+	struct peer *p = alloc_peer(TOO_MUCH_DATA);
+	BOOST_REQUIRE(p != NULL);
+
+	char *read_ptr = get_read_ptr(p, MAX_MESSAGE_SIZE + 1);
+	BOOST_CHECK(read_ptr == NULL);
+
+	free_peer(p);
+}
+
+BOOST_AUTO_TEST_CASE(client_closed_connection)
+{
+	struct peer *p = alloc_peer(CLIENT_CLOSE);
+	BOOST_REQUIRE(p != NULL);
+
+	char *read_ptr = get_read_ptr(p, MAX_MESSAGE_SIZE);
+	BOOST_CHECK(read_ptr == NULL);
+
+	free_peer(p);
+}
+
+BOOST_AUTO_TEST_CASE(eagain)
+{
+	struct peer *p = alloc_peer(AGAIN);
+	BOOST_REQUIRE(p != NULL);
+
+	char *read_ptr = get_read_ptr(p, MAX_MESSAGE_SIZE);
+	BOOST_CHECK(read_ptr == (char *)-1);
+
+	free_peer(p);
+}
