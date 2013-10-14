e2a17d9b50335e11f6246086ee3ae909c2cc0fd5
Stephan Gatzka <stephan.gatzka@gmail.com>
2013-10-14 21:49:51 +0200

    Add trampoline code to call function withn respect to the incoming JSON method.

--- parse.c
+++ parse.c
@@ -1,5 +1,6 @@
 #include <stdint.h>
 #include <stdio.h>
+#include <string.h>
 
 #include "compiler.h"
 #include "cJSON.h"
@@ -15,12 +16,47 @@ int parse_message(const char *msg, uint32_t length)
 		fprintf(stderr, "Could not parse JSON!\n");
 		return -1;
 	} else {
+		cJSON *method;
+		const char *method_string;
 		uint32_t parsed_length = end_parse - msg;
 		if (unlikely(parsed_length != length)) {
 			fprintf(stderr, "length of parsed JSON does not match message length!\n");
 			return -1;
 		}
+		method = cJSON_GetObjectItem(root, "method");
+		if (unlikely(method == NULL)) {
+			goto no_method;
+		}
+		method_string = method->valuestring;
+		if (unlikely(method_string == NULL)) {
+			goto no_method;
+		}
+
+		if (strcmp(method_string, "set") == 0) {
+
+		} else if (strcmp(method_string, "post") == 0) {
+
+		} else if (strcmp(method_string, "add") == 0) {
+
+		} else if (strcmp(method_string, "remove") == 0) {
+
+		} else if (strcmp(method_string, "call") == 0) {
+
+		} else if (strcmp(method_string, "fetch") == 0) {
+
+		} else if (strcmp(method_string, "unfetch") == 0) {
+
+		} else {
+			fprintf(stderr, "Unsupported method: %s!\n", method_string);
+			goto unsupported_method;
+		}
 		cJSON_Delete(root);
 		return 0;
+
+	no_method:
+		fprintf(stderr, "Can not find supported method!\n");
+	unsupported_method:
+		cJSON_Delete(root);
+		return -1;
 	}
 }
