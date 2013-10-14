4b0a5378aafe33511560c259a9a67589acca6783
Stephan Gatzka <stephan.gatzka@gmail.com>
2013-10-14 21:07:30 +0200

    Take out copyright notice until I've decided about the license.

--- tests/Parse.cpp
+++ tests/Parse.cpp
@@ -1,31 +1,3 @@
-/*
- * Copyright (C) 2007 Hottinger Baldwin Messtechnik GmbH
- * Im Tiefen See 45
- * 64293 Darmstadt
- * Germany
- * http://www.hbm.com
- * All rights reserved
- *
- * The copyright to the computer program(s) herein is the property of
- * Hottinger Baldwin Messtechnik GmbH (HBM), Germany. The program(s)
- * may be used and/or copied only with the written permission of HBM
- * or in accordance with the terms and conditions stipulated in the
- * agreement/contract under which the program(s) have been supplied.
- * This copyright notice must not be removed.
- *
- * This Software is licenced by the
- * "General supply and license conditions for software"
- * which is part of the standard terms and conditions of sale from HBM.
-*/
-
-/*
-$HeadURL:$
-$Revision:$
-$Author:$
-$Date:$
-*/
-/** @file */
-
 #define BOOST_TEST_DYN_LINK
 #define BOOST_TEST_MAIN
 #define BOOST_TEST_MODULE parse JSON
