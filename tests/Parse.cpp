41bda31827e1053feefb4ab9da49c2509613d557
Stephan Gatzka <stephan.gatzka@gmail.com>
2013-10-14 21:49:24 +0200

    Add more unit tests.

--- tests/Parse.cpp
+++ tests/Parse.cpp
@@ -7,6 +7,9 @@
 
 static const char correct_json[] = "{\"id\": 7384,\"method\": \"add\",\"params\":{\"path\": \"foo/bar/state\",\"value\": 123}}";
 static const char wrong_json[] = "{\"id\": 7384,\"method\": add\",\"params\":{\"path\": \"foo/bar/state\",\"value\": 123}}";
+static const char json_no_method[] = "{\"id\": 7384,\"meth\": \"add\",\"params\":{\"path\": \"foo/bar/state\",\"value\": 123}}";
+static const char json_no_string_method[] = "{\"id\": 7384,\"method\": 123,\"params\":{\"path\": \"foo/bar/state\",\"value\": 123}}";
+static const char json_unsupported_method[] = "{\"id\": 7384,\"method\": \"horst\",\"params\":{\"path\": \"foo/bar/state\",\"value\": 123}}";
 
 BOOST_AUTO_TEST_CASE(parse_correct_json)
 {
@@ -29,5 +32,23 @@ BOOST_AUTO_TEST_CASE(lenth_too_short)
 BOOST_AUTO_TEST_CASE(parse_wrong_json)
 {
 	int ret = parse_message(wrong_json, strlen(wrong_json));
+	BOOST_CHECK(ret == -1);
+}
+
+BOOST_AUTO_TEST_CASE(no_method)
+{
+	int ret = parse_message(json_no_method, strlen(json_no_method));
+	BOOST_CHECK(ret == -1);
+}
+
+BOOST_AUTO_TEST_CASE(no_string_method)
+{
+	int ret = parse_message(json_no_string_method, strlen(json_no_string_method));
+	BOOST_CHECK(ret == -1);
+}
+
+BOOST_AUTO_TEST_CASE(unsupported_method)
+{
+	int ret = parse_message(json_unsupported_method, strlen(json_unsupported_method));
 	BOOST_CHECK(ret == -1);
 }
