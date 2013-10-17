eb395a51fe848d19c3ccd7034544e67df68b7456
Stephan Gatzka <stephan.gatzka@gmail.com>
2013-10-17 18:30:57 +0200

    Add compile switch -Wwrite-strings.
    This is done to warn about attemps to write into string constants.

--- clang.cmake
+++ clang.cmake
@@ -1,7 +1,7 @@
 SET (CMAKE_C_COMPILER        "clang")
 SET (CMAKE_CXX_COMPILER      "clang++")
 
-SET(CMAKE_C_FLAGS "-pipe -Wall -Wextra -pedantic -O2 -flto -fomit-frame-pointer" CACHE STRING "" FORCE)
+SET(CMAKE_C_FLAGS "-pipe -Wall -Wextra -Wwrite-strings -pedantic -O2 -flto -fomit-frame-pointer" CACHE STRING "" FORCE)
 
 MESSAGE (STATUS "Found CLANG toolchain")
 
--- gcc.cmake
+++ gcc.cmake
@@ -3,7 +3,7 @@ SET (CMAKE_CXX_COMPILER      "clang++")
 SET (CMAKE_C_COMPILER "gcc")
 SET (CMAKE_CXX_COMPILER "g++")
 
-SET(CMAKE_C_FLAGS "-pipe -Wall -Wextra -pedantic -O2 -flto -fwhole-program -fomit-frame-pointer" CACHE STRING "" FORCE)
+SET(CMAKE_C_FLAGS "-pipe -Wall -Wextra -Wwrite-strings -pedantic -O2 -flto -fwhole-program -fomit-frame-pointer" CACHE STRING "" FORCE)
 # SET(CMAKE_C_FLAGS "-pipe -Wall -Wextra -pedantic -O2 -fomit-frame-pointer" CACHE STRING "" FORCE)
 # SET(CMAKE_C_FLAGS "-pipe -Wall -Wextra -pedantic -O2 -flto -fuse-linker-plugin -fomit-frame-pointer" CACHE STRING "" FORCE)
 
