a39424d053ff6249da0dd8142016bfcd7e83f881
Stephan Gatzka <stephan.gatzka@gmail.com>
2013-10-17 19:42:21 +0200

    Don't use link time optimization.
    This failes at least for the tests.

--- clang.cmake
+++ clang.cmake
@@ -1,7 +1,8 @@
 SET (CMAKE_C_COMPILER        "clang")
 SET (CMAKE_CXX_COMPILER      "clang++")
 
-SET(CMAKE_C_FLAGS "-pipe -Wall -Wextra -Wwrite-strings -pedantic -O2 -flto -fomit-frame-pointer" CACHE STRING "" FORCE)
+SET(CMAKE_C_FLAGS "-pipe -Wall -Wextra -Wwrite-strings -pedantic -O2 -fomit-frame-pointer" CACHE STRING "" FORCE)
+# SET(CMAKE_C_FLAGS "-pipe -Wall -Wextra -Wwrite-strings -pedantic -O2 -flto -use-gold-plugin -fomit-frame-pointer" CACHE STRING "" FORCE)
 
 MESSAGE (STATUS "Found CLANG toolchain")
 
