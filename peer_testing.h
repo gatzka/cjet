237c7004130c08be3ea1a06d0e5ac76b1a153e5a
Stephan Gatzka <stephan.gatzka@gmail.com>
2013-10-17 19:33:10 +0200

    Add "extern C" stuff for clean compile of tests.

--- peer_testing.h
+++ peer_testing.h
@@ -2,9 +2,21 @@
 #define CJET_PEER_TESTING_H
 
 #ifdef TESTING
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+ssize_t fake_read(int fd, void *buf, size_t count);
+
+#ifdef __cplusplus
+}
+#endif
+
 #define READ \
 	fake_read
+
 #else
+
 #define READ \
 	read
 #endif
