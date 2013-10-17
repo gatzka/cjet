2ce4718b64754eadb1559e7096a8ed4b9f5442c7
Stephan Gatzka <stephan.gatzka@gmail.com>
2013-10-17 20:00:31 +0200

    Add tests for JSON-RPC arrays.

--- tests/Parse.cpp
+++ tests/Parse.cpp
@@ -9,7 +9,10 @@ static const char correct_json[] = "{\"id\": 7384,\"method\": \"add\",\"params\"
 static const char wrong_json[] = "{\"id\": 7384,\"method\": add\",\"params\":{\"path\": \"foo/bar/state\",\"value\": 123}}";
 static const char json_no_method[] = "{\"id\": 7384,\"meth\": \"add\",\"params\":{\"path\": \"foo/bar/state\",\"value\": 123}}";
 static const char json_no_string_method[] = "{\"id\": 7384,\"method\": 123,\"params\":{\"path\": \"foo/bar/state\",\"value\": 123}}";
+static const char json_two_method[] = "[{\"id\": 7384,\"method\": \"add\",\"params\":{\"path\": \"foo/bar/state\",\"value\": 123}}, {\"id\": 7384,\"method\": \"add\",\"params\":{\"path\": \"foo/state\",\"value\": 321}}]";
 static const char json_unsupported_method[] = "{\"id\": 7384,\"method\": \"horst\",\"params\":{\"path\": \"foo/bar/state\",\"value\": 123}}";
+static const char wrong_jet_array[] = "[1, 2]";
+
 
 BOOST_AUTO_TEST_CASE(parse_correct_json)
 {
@@ -50,5 +53,17 @@ BOOST_AUTO_TEST_CASE(no_string_method)
 BOOST_AUTO_TEST_CASE(unsupported_method)
 {
 	int ret = parse_message(json_unsupported_method, strlen(json_unsupported_method));
+	BOOST_CHECK(ret == -1);
+}
+
+BOOST_AUTO_TEST_CASE(two_method)
+{
+	int ret = parse_message(json_two_method, strlen(json_two_method));
+	BOOST_CHECK(ret == 0);
+}
+
+BOOST_AUTO_TEST_CASE(wrong_array)
+{
+	int ret = parse_message(wrong_jet_array, strlen(wrong_jet_array));
 	BOOST_CHECK(ret == -1);
 }
