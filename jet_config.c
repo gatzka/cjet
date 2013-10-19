86db143112379b4847bf329e99e402294938d446
Stephan Gatzka <stephan.gatzka@gmail.com>
2013-10-19 13:14:53 +0200

    Destroy JSON object after using it.

--- jet_config.c
+++ jet_config.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 #include "cJSON.h"
+#include "compiler.h"
 #include "jet_config.h"
 #include "peer.h"
 #include "response.h"
@@ -10,6 +11,10 @@ int process_config(cJSON *json_rpc, struct peer *p)
 	cJSON *id = cJSON_GetObjectItem(json_rpc, "id");
 	if (id != NULL) {
 		cJSON *response = create_boolean_success_response(id, 1);
+		if (unlikely (response == NULL)) {
+			return -1;
+		}
+		cJSON_Delete(response);
 	}
 	return 0;
 }
