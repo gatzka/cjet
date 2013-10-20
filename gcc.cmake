2540be314438b56d8357527605e74d7e3506169c
Stephan Gatzka <stephan.gatzka@gmail.com>
2013-10-20 12:54:20 +0200

    Add some linker optimizations.

--- gcc.cmake
+++ gcc.cmake
@@ -1,21 +1,10 @@
-SET (CMAKE_C_COMPILER        "clang")
-SET (CMAKE_CXX_COMPILER      "clang++")
 SET (CMAKE_C_COMPILER "gcc")
 SET (CMAKE_CXX_COMPILER "g++")
+SET (CMAKE_EXE_LINKER_FLAGS "-Wl,-O2 -Wl,--hash-style=gnu -Wl,--as-needed -Wl,--gc-sections" CACHE STRING "" FORCE)
 
 SET(CMAKE_C_FLAGS "-pipe -Wall -Wextra -Wwrite-strings -pedantic -O2 -flto -fwhole-program -fomit-frame-pointer" CACHE STRING "" FORCE)
 # SET(CMAKE_C_FLAGS "-pipe -Wall -Wextra -pedantic -O2 -fomit-frame-pointer" CACHE STRING "" FORCE)
 # SET(CMAKE_C_FLAGS "-pipe -Wall -Wextra -pedantic -O2 -flto -fuse-linker-plugin -fomit-frame-pointer" CACHE STRING "" FORCE)
 
-
-
 MESSAGE (STATUS "Found gcc toolchain")
-
-###set (BOOST_ROOT /opt/boost-1.52.0/)
-# adjust the default behaviour of the FIND_XXX() commands:
-# search headers and libraries in the target environment, search
-# programs in the host environment
-#set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
-#set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
-#set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 
