5265a83995d813fe0d12fbfbd1d71d216016c989
Stephan Gatzka <stephan.gatzka@gmail.com>
2013-10-20 12:53:50 +0200

    Remove uncommented stuff.

--- clang.cmake
+++ clang.cmake
@@ -6,11 +6,3 @@ SET(CMAKE_C_FLAGS "-pipe -Wall -Wextra -Wwrite-strings -pedantic -O2 -fomit-fram
 
 MESSAGE (STATUS "Found CLANG toolchain")
 
-###set (BOOST_ROOT /opt/boost-1.52.0/)
-# adjust the default behaviour of the FIND_XXX() commands:
-# search headers and libraries in the target environment, search
-# programs in the host environment
-#set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
-#set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
-#set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
-
