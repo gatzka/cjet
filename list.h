94982d4ec6572d46a059ec70a631f5efe22aaf83
Stephan Gatzka <stephan.gatzka@gmail.com>
2013-10-20 20:09:12 +0200

    Add list_add_tail function.

--- list.h
+++ list.h
@@ -11,4 +11,18 @@ static inline void INIT_LIST_HEAD(struct list_head *list)
 	list->prev = list;
 }
 
+static inline void __list_add(struct list_head *new, struct list_head *prev, struct list_head *next)
+{
+	next->prev = new;
+	new->next = next;
+	new->prev = prev;
+	prev->next = new;
+}
+
+static inline void list_add_tail(struct list_head *new, struct list_head *head)
+{
+	__list_add(new, head->prev, head);
+}
+
+
 #endif
