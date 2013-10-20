b9455cfde03c6e3707d7ac42da17c64b27408b69
Stephan Gatzka <stephan.gatzka@gmail.com>
2013-10-20 12:57:51 +0200

    Add some linker optimizations.

--- clang.cmake
+++ clang.cmake
@@ -2,6 +2,7 @@ SET (CMAKE_C_COMPILER        "clang")
 SET (CMAKE_CXX_COMPILER      "clang++")
 
 SET(CMAKE_C_FLAGS "-pipe -Wall -Wextra -Wwrite-strings -pedantic -O2 -fomit-frame-pointer" CACHE STRING "" FORCE)
+SET (CMAKE_EXE_LINKER_FLAGS "-Wl,-O2 -Wl,--hash-style=gnu -Wl,--as-needed -Wl,--gc-sections" CACHE STRING "" FORCE)
 # SET(CMAKE_C_FLAGS "-pipe -Wall -Wextra -Wwrite-strings -pedantic -O2 -flto -use-gold-plugin -fomit-frame-pointer" CACHE STRING "" FORCE)
 
 MESSAGE (STATUS "Found CLANG toolchain")
