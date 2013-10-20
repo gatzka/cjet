683f41865ad3915450735cacea24e45bbae7d42b
Stephan Gatzka <stephan.gatzka@gmail.com>
2013-10-20 20:11:21 +0200

    Add function to allocate a state.

--- state.c
+++ state.c
@@ -1,6 +1,20 @@
+#include <stdlib.h>
+
 #include "cJSON.h"
+#include "compiler.h"
 #include "peer.h"
 #include "state.h"
+
+static struct state *alloc_state() {
+	struct state *s;
+	s = malloc(sizeof(*s));
+	if (unlikely(s == NULL)) {
+		return NULL;
+	}
+	INIT_LIST_HEAD(&s->next_state);
+
+	return s;
+}
 
 int add_state_to_peer(struct peer *p, const char *path, cJSON *value)
 {
