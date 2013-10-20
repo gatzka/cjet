b13bf80e0703023dcec06196a2c8d0082b239881
Stephan Gatzka <stephan.gatzka@gmail.com>
2013-10-20 20:10:24 +0200

    Add preliminary state struct.

--- state.h
+++ state.h
@@ -2,7 +2,14 @@
 #define CJET_STATE_H
 
 #include "cJSON.h"
+#include "list.h"
 #include "peer.h"
+
+struct state {
+	struct list_head next_state;
+	const char *path;
+	cJSON *value;
+};
 
 int add_state_to_peer(struct peer *p, const char *path, cJSON *value);
 
