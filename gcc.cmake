af8a9251dcc3cab4a2a30c4de5e701cd88ab972b
Stephan Gatzka <stephan.gatzka@gmail.com>
2013-10-24 12:20:20 +0200

    Add compile switch to warn about shadowed variables or functions.

--- gcc.cmake
+++ gcc.cmake
@@ -1,7 +1,7 @@
 SET (CMAKE_C_COMPILER "gcc")
 SET (CMAKE_CXX_COMPILER "g++")
 
-SET(CMAKE_C_FLAGS "-pipe -Wall -Wextra -Wwrite-strings -std=c99 -pedantic -O2 -flto -fwhole-program -fomit-frame-pointer" CACHE STRING "" FORCE)
+SET(CMAKE_C_FLAGS "-pipe -Wall -Wshadow -Wextra -Wwrite-strings -std=c99 -pedantic -O2 -flto -fwhole-program -fomit-frame-pointer" CACHE STRING "" FORCE)
 #SET(CMAKE_C_FLAGS "-pipe -Wall -Wextra -Wwrite-strings -std=c99 -pedantic -O0 -ggdb" CACHE STRING "" FORCE)
 SET (CMAKE_EXE_LINKER_FLAGS "-Wl,-O2 -Wl,--hash-style=gnu -Wl,--as-needed -Wl,--gc-sections" CACHE STRING "" FORCE)
 
