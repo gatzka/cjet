61c8a6175b2d32b068bb5d264281334b28917d9a
Stephan Gatzka <stephan.gatzka@gmail.com>
2013-10-24 23:01:29 +0200

    Add debug and release targets.

--- clang.cmake
+++ clang.cmake
@@ -1,8 +1,16 @@
 SET (CMAKE_C_COMPILER "clang")
 SET (CMAKE_CXX_COMPILER "clang++")
 
-SET(CMAKE_C_FLAGS "-pipe -Wall -Wextra -Wshadow -Wformat=2 -Wwrite-strings -std=c99 -pedantic -O2 -fomit-frame-pointer" CACHE STRING "" FORCE)
-SET (CMAKE_EXE_LINKER_FLAGS "-Wl,-O2 -Wl,--hash-style=gnu -Wl,--as-needed -Wl,--gc-sections" CACHE STRING "" FORCE)
+SET(CMAKE_C_FLAGS "-pipe -Wall -Wextra -Wshadow -Wformat=2 -Wwrite-strings -std=c99 -pedantic" CACHE STRING "" FORCE)
+
+SET(CMAKE_C_FLAGS_RELEASE "-O2 -fomit-frame-pointer" CACHE STRING "" FORCE)
+SET(CMAKE_C_FLAGS_DEBUG "-O0 -ggdb" CACHE STRING "" FORCE)
+
+IF(NOT CMAKE_BUILD_TYPE )
+  SET(CMAKE_BUILD_TYPE RELEASE CACHE STRING "" FORCE )
+ENDIF()
+
+SET(CMAKE_EXE_LINKER_FLAGS "-Wl,-O2 -Wl,--hash-style=gnu -Wl,--as-needed -Wl,--gc-sections" CACHE STRING "" FORCE)
 
 MESSAGE (STATUS "Found CLANG toolchain")
 
--- gcc.cmake
+++ gcc.cmake
@@ -1,8 +1,14 @@
 SET (CMAKE_C_COMPILER "gcc")
 SET (CMAKE_CXX_COMPILER "g++")
 
-SET(CMAKE_C_FLAGS "-pipe -Wall -Wshadow -Wextra -Wformat=2 -Wwrite-strings -std=c99 -pedantic -O2 -flto -fwhole-program -fomit-frame-pointer" CACHE STRING "" FORCE)
-#SET(CMAKE_C_FLAGS "-pipe -Wall -Wextra -Wwrite-strings -std=c99 -pedantic -O0 -ggdb" CACHE STRING "" FORCE)
+SET(CMAKE_C_FLAGS "-pipe -Wall -Wshadow -Wextra -Wformat=2 -Wwrite-strings -Wmissing-prototypes -std=c99 -pedantic" CACHE STRING "" FORCE)
+SET(CMAKE_C_FLAGS_RELEASE "-O2 -flto -fwhole-program -fomit-frame-pointer" CACHE STRING "" FORCE)
+SET(CMAKE_C_FLAGS_DEBUG "-O0 -ggdb" CACHE STRING "" FORCE)
+
+IF(NOT CMAKE_BUILD_TYPE )
+  SET(CMAKE_BUILD_TYPE RELEASE CACHE STRING "" FORCE )
+ENDIF()
+
 SET (CMAKE_EXE_LINKER_FLAGS "-Wl,-O2 -Wl,--hash-style=gnu -Wl,--as-needed -Wl,--gc-sections" CACHE STRING "" FORCE)
 
 MESSAGE (STATUS "Found gcc toolchain")
