d565fd73dbd40f0cce6807ddd3ea0ae4f5ba546c
Stephan Gatzka <stephan.gatzka@gmail.com>
2013-10-24 19:30:51 +0200

    Enable security related format string warnings.

--- clang.cmake
+++ clang.cmake
@@ -1,7 +1,7 @@
 SET (CMAKE_C_COMPILER "clang")
 SET (CMAKE_CXX_COMPILER "clang++")
 
-SET(CMAKE_C_FLAGS "-pipe -Wall -Wextra -Wshadow -Wwrite-strings -std=c99 -pedantic -O2 -fomit-frame-pointer" CACHE STRING "" FORCE)
+SET(CMAKE_C_FLAGS "-pipe -Wall -Wextra -Wshadow -Wformat=2 -Wwrite-strings -std=c99 -pedantic -O2 -fomit-frame-pointer" CACHE STRING "" FORCE)
 SET (CMAKE_EXE_LINKER_FLAGS "-Wl,-O2 -Wl,--hash-style=gnu -Wl,--as-needed -Wl,--gc-sections" CACHE STRING "" FORCE)
 
 MESSAGE (STATUS "Found CLANG toolchain")
