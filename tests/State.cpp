33971ac951a3315e452eadb05e8cad146fa35868
Stephan Gatzka <stephan.gatzka@gmail.com>
2013-10-26 10:27:39 +0200

    Add unit test for adding a state twice.

--- tests/State.cpp
+++ tests/State.cpp
@@ -39,3 +39,30 @@ BOOST_AUTO_TEST_CASE(add_state)
 	delete_setter_hashtable();
 }
 
+BOOST_AUTO_TEST_CASE(add_duplicate_state)
+{
+	create_setter_hashtable();
+	struct peer *p = alloc_peer(-1);
+
+	cJSON *value = cJSON_CreateNumber(1234);
+
+	cJSON *error = add_state_to_peer(p, "/foo/bar/", value);
+	BOOST_CHECK(error == NULL);
+
+	error = add_state_to_peer(p, "/foo/bar/", value);
+	BOOST_CHECK(error != NULL);
+	cJSON *code = cJSON_GetObjectItem(error, "code");
+	BOOST_REQUIRE(code != NULL);
+	BOOST_CHECK(code->type == cJSON_Number);
+	BOOST_CHECK(code->valueint == -32602);
+	cJSON *message = cJSON_GetObjectItem(error, "message");
+	BOOST_REQUIRE(message != NULL);
+	BOOST_CHECK(message->type == cJSON_String);
+	BOOST_CHECK(strcmp(message->valuestring, "Invalid params") == 0);
+
+	cJSON_Delete(error);
+
+	cJSON_Delete(value);
+	free_peer(p);
+	delete_setter_hashtable();
+}
