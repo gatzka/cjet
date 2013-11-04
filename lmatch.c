67c39fff985c4c4c2654c60260926a766d4d2678
Stephan Gatzka <stephan.gatzka@gmail.com>
2013-11-04 08:42:55 +0100

    Take out all capture and character class stuff.

--- lmatch.c
+++ lmatch.c
@@ -35,8 +35,8 @@
 #define CAP_UNFINISHED  (-1)
 #define CAP_POSITION    (-2)
 
-#define L_ESC       '%'
-#define SPECIALS    "^$*+?.([%-"
+
+#define SPECIALS    "^$*+?.-"
 
 typedef struct MatchState {
 	int matchdepth;  /* control for recursive depth (to avoid C stack overflow) */
@@ -76,164 +76,9 @@ static int nospecials(const char *p)
 		return 1;
 }
 
-#if 0
-static int nospecials(const char *p, size_t l) {
-	size_t upto = 0;
-	do {
-		if (strpbrk(p + upto, SPECIALS))
-			return 0;  /* pattern has a special character */
-		upto += strlen(p + upto) + 1;  /* may have more after \0 */
-	} while (upto <= l);
-	return 1;  /* no special chars found */
-}
-#endif
-
 static const char *match(MatchState *ms, const char *s, const char *p);
 
-#if 0
-static int capture_to_close(MatchState *ms)
-{
-	int level = ms->level;
-	for (level--; level>=0; level--)
-		if (ms->capture[level].len == CAP_UNFINISHED) return level;
-	printf("invalid pattern capture");
-	return 0;
-}
-
-static const char *start_capture(MatchState *ms, const char *s,	const char *p, int what)
-{
-	const char *res;
-	int level = ms->level;
-	if (level >= LUA_MAXCAPTURES) printf("too many captures");
-	ms->capture[level].init = s;
-	ms->capture[level].len = what;
-	ms->level = level+1;
-	if ((res=match(ms, s, p)) == NULL)  /* match failed? */
-		ms->level--;  /* undo capture */
-	return res;
-}
-
-static const char *end_capture (MatchState *ms, const char *s, const char *p)
-{
-	int l = capture_to_close(ms);
-	const char *res;
-	ms->capture[l].len = s - ms->capture[l].init;  /* close capture */
-	if ((res = match(ms, s, p)) == NULL)  /* match failed? */
-		ms->capture[l].len = CAP_UNFINISHED;  /* undo capture */
-	return res;
-}
-
-static const char *matchbalance(MatchState *ms, const char *s,	const char *p)
-{
-	if (p >= ms->p_end - 1)
-		printf("malformed pattern (missing arguments\n");
-	if (*s != *p) return NULL;
-	else {
-		int b = *p;
-		int e = *(p+1);
-		int cont = 1;
-		while (++s < ms->src_end) {
-			if (*s == e) {
-				if (--cont == 0) return s+1;
-			}
-			else if (*s == b) cont++;
-		}
-	}
-	return NULL;  /* string ends out of balance */
-}
-
-#endif
-
-static const char *classend(MatchState *ms, const char *p)
-{
-	switch (*p++) {
-		case L_ESC: {
-						if (p == ms->p_end)
-							printf("malformed pattern (ends with )\n");
-						return p+1;
-					}
-		case '[': {
-					  if (*p == '^') p++;
-					  do {  /* look for a `]' */
-						  if (p == ms->p_end)
-							  printf("malformed pattern (missing ]\n");
-						  if (*(p++) == L_ESC && p < ms->p_end)
-							  p++;  /* skip escapes (e.g. `%]') */
-					  } while (*p != ']');
-					  return p+1;
-				  }
-		default: {
-					 return p;
-				 }
-	}
-}
-
-#if 0
-static int check_capture(MatchState *ms, int l)
-{
-	l -= '1';
-	if (l < 0 || l >= ms->level || ms->capture[l].len == CAP_UNFINISHED)
-		printf("invalid capture index %d\n", l + 1);
-		return 0;
-	return l;
-}
-
-static const char *match_capture(MatchState *ms, const char *s, int l)
-{
-	size_t len;
-	l = check_capture(ms, l);
-	len = ms->capture[l].len;
-	if ((size_t)(ms->src_end-s) >= len &&
-			memcmp(ms->capture[l].init, s, len) == 0)
-		return s+len;
-	else return NULL;
-}
-#endif
-
-static int match_class(int c, int cl)
-{
-	int res;
-	switch (tolower(cl)) {
-		case 'a' : res = isalpha(c); break;
-		case 'c' : res = iscntrl(c); break;
-		case 'd' : res = isdigit(c); break;
-		case 'g' : res = isgraph(c); break;
-		case 'l' : res = islower(c); break;
-		case 'p' : res = ispunct(c); break;
-		case 's' : res = isspace(c); break;
-		case 'u' : res = isupper(c); break;
-		case 'w' : res = isalnum(c); break;
-		case 'x' : res = isxdigit(c); break;
-		case 'z' : res = (c == 0); break;  /* deprecated option */
-		default: return (cl == c);
-	}
-	return (islower(cl) ? res : !res);
-}
-
-static int matchbracketclass(int c, const char *p, const char *ec)
-{
-	int sig = 1;
-	if (*(p+1) == '^') {
-		sig = 0;
-		p++;  /* skip the `^' */
-	}
-	while (++p < ec) {
-		if (*p == L_ESC) {
-			p++;
-			if (match_class(c, uchar(*p)))
-				return sig;
-		}
-		else if ((*(p+1) == '-') && (p+2 < ec)) {
-			p+=2;
-			if (uchar(*(p-2)) <= c && c <= uchar(*p))
-				return sig;
-		}
-		else if (uchar(*p) == c) return sig;
-	}
-	return !sig;
-}
-
-static int singlematch(MatchState *ms, const char *s, const char *p, const char *ep)
+static int singlematch(MatchState *ms, const char *s, const char *p)
 {
 	if (s >= ms->src_end)
 		return 0;
@@ -241,8 +86,6 @@ static int singlematch(MatchState *ms, const char *s, const char *p, const char 
 		int c = uchar(*s);
 		switch (*p) {
 			case '.': return 1;  /* matches any char */
-			case L_ESC: return match_class(c, uchar(*(p+1)));
-			case '[': return matchbracketclass(c, p, ep-1);
 			default:  return (uchar(*p) == c);
 		}
 	}
@@ -251,7 +94,7 @@ static int singlematch(MatchState *ms, const char *s, const char *p, const char 
 static const char *max_expand(MatchState *ms, const char *s, const char *p, const char *ep)
 {
 	ptrdiff_t i = 0;  /* counts maximum expand for item */
-	while (singlematch(ms, s + i, p, ep))
+	while (singlematch(ms, s + i, p))
 		i++;
 	/* keeps trying to match with the maximum repetitions */
 	while (i>=0) {
@@ -262,13 +105,13 @@ static const char *max_expand(MatchState *ms, const char *s, const char *p, cons
 	return NULL;
 }
 
-static const char *min_expand (MatchState *ms, const char *s, const char *p, const char *ep)
+static const char *min_expand(MatchState *ms, const char *s, const char *p, const char *ep)
 {
 	for (;;) {
 		const char *res = match(ms, s, ep+1);
 		if (res != NULL)
 			return res;
-		else if (singlematch(ms, s, p, ep))
+		else if (singlematch(ms, s, p))
 			s++;  /* try with one more repetition */
 		else return NULL;
 	}
@@ -281,67 +124,17 @@ static const char *match(MatchState *ms, const char *s, const char *p)
 init: /* using goto's to optimize tail recursion */
 	if (p != ms->p_end) {  /* end of pattern? */
 		switch (*p) {
-#if 0
-			case '(': {  /* start capture */
-						  if (*(p + 1) == ')')  /* position capture? */
-							  s = start_capture(ms, s, p + 2, CAP_POSITION);
-						  else
-							  s = start_capture(ms, s, p + 1, CAP_UNFINISHED);
-						  break;
-					  }
-			case ')': {  /* end capture */
-						  s = end_capture(ms, s, p + 1);
-						  break;
-					  }
-#endif
 			case '$': {
 						  if ((p + 1) != ms->p_end)  /* is the `$' the last char in pattern? */
 							  goto dflt;  /* no; go to default */
 						  s = (s == ms->src_end) ? s : NULL;  /* check end of string */
 						  break;
 					  }
-#if 0
-			case L_ESC: {  /* escaped sequences not in the format class[*+?-]? */
-							switch (*(p + 1)) {
-								case 'b': {  /* balanced string? */
-											  s = matchbalance(ms, s, p + 2);
-											  if (s != NULL) {
-												  p += 4; goto init;  /* return match(ms, s, p + 4); */
-											  }  /* else fail (s == NULL) */
-											  break;
-										  }
-								case 'f': {  /* frontier? */
-											  const char *ep; char previous;
-											  p += 2;
-											  if (*p != '[')
-												  printf("missing [ after f in pattern\n");
-											  ep = classend(ms, p);  /* points to what is next */
-											  previous = (s == ms->src_init) ? '\0' : *(s - 1);
-											  if (!matchbracketclass(uchar(previous), p, ep - 1) &&
-													  matchbracketclass(uchar(*s), p, ep - 1)) {
-												  p = ep; goto init;  /* return match(ms, s, ep); */
-											  }
-											  s = NULL;  /* match failed */
-											  break;
-										  }
-								case '0': case '1': case '2': case '3':
-								case '4': case '5': case '6': case '7':
-								case '8': case '9': {  /* capture results (%0-%9)? */
-														s = match_capture(ms, s, uchar(*(p + 1)));
-														if (s != NULL) {
-															p += 2; goto init;  /* return match(ms, s, p + 2) */
-														}
-														break;
-													}
-								default: goto dflt;
-							}
-							break;
-						}
-#endif
 			default: dflt: {  /* pattern class plus optional suffix */
-						 const char *ep = classend(ms, p);  /* points to optional suffix */
+						 const char *ep = p;  /* points to optional suffix */
+						 ep++;
 						 /* does not match at least once? */
-						 if (!singlematch(ms, s, p, ep)) {
+						 if (!singlematch(ms, s, p)) {
 							 if (*ep == '*' || *ep == '?' || *ep == '-') {  /* accept empty? */
 								 p = ep + 1; goto init;  /* return match(ms, s, ep + 1); */
 							 }
