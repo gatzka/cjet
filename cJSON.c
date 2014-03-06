6c87ede369fe50f2a0bde5e571a9144141759709
Stephan Gatzka <stephan.gatzka@gmail.com>
2014-03-06 17:49:03 +0100

    Take out check of len variable in condition.
    The check is not necessary because len is initialized with 0
    and always incremented before the check. So len is never 0
    (if there is no overflow).

--- cJSON.c
+++ cJSON.c
@@ -323,7 +323,8 @@ static char *print_string_ptr(const char *str)
 	if (!str)
 		return cJSON_strdup("");
 	ptr = str;
-	while ((token = *ptr) && ++len) {
+	while ((token = *ptr)) {
+		++len;
 		if (strchr("\"\\\b\f\n\r\t", token))
 			len++;
 		else if (token < 32)
