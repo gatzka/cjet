a9af54a99cfc8e1625bf1512faebe79f81e4cdc1
Stephan Gatzka <stephan.gatzka@gmail.com>
2014-03-15 19:53:29 +0100

    Move IO specific stuff into separate files.

--- linux/main_mt.c
+++ linux/main_mt.c
@@ -1,157 +1,16 @@
-#include <arpa/inet.h>
-#include <errno.h>
-#include <netinet/in.h>
-#include <netinet/tcp.h>
-#include <pthread.h>
 #include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <sys/socket.h>
-#include <sys/types.h>
-#include <unistd.h>
 
-#include "../compiler.h"
-#include "../peer.h"
 #include "../state.h"
-#include "config.h"
+#include "io.h"
 
 static int shall_close = 0;
-
-static void close_peer_connection(struct peer *p, int fd)
-{
-	close(fd);
-	free_peer(p);
-}
-
-static struct peer *setup_listen_socket()
-{
-	int listen_fd;
-	struct sockaddr_in6 serveraddr;
-	static const int reuse_on = 1;
-	struct peer *peer;
-
-	if ((listen_fd = socket(AF_INET6, SOCK_STREAM, 0)) < 0) {
-		fprintf(stderr, "Could not create listen socket!\n");
-		return NULL;
-	}
-
-	if (setsockopt(listen_fd, SOL_SOCKET, SO_REUSEADDR, &reuse_on, sizeof(reuse_on)) < 0) {
-		fprintf(stderr, "Could not set SO_REUSEADDR!\n");
-		goto so_reuse_failed;
-	}
-
-	memset(&serveraddr, 0, sizeof(serveraddr));
-	serveraddr.sin6_family = AF_INET6;
-	serveraddr.sin6_port = htons(SERVER_PORT);
-	serveraddr.sin6_addr = in6addr_any;
-	if (bind(listen_fd, (struct sockaddr *)&serveraddr, sizeof(serveraddr)) < 0) {
-		fprintf(stderr, "bind failed!\n");
-		goto bind_failed;
-	}
-
-	if (listen(listen_fd, LISTEN_BACKLOG) < 0) {
-		fprintf(stderr, "listen failed!\n");
-		goto listen_failed;
-	}
-
-	peer = alloc_peer(listen_fd);
-	if (peer == NULL) {
-		goto alloc_peer_failed;
-	}
-
-	return peer;
-
-alloc_peer_failed:
-listen_failed:
-bind_failed:
-so_reuse_failed:
-	close(listen_fd);
-	return NULL;
-}
-
-static void *handle_client(void *arg)
-{
-	struct peer *peer = arg;
-
-	int ret = handle_all_peer_operations(peer);
-	if (unlikely(ret == -1)) {
-		close_peer_connection(peer, peer->fd);
-	}
-	return NULL;
-}
 
 static void sighandler(int signum)
 {
 	(void)signum;
 	shall_close = 1;
-}
-
-static inline int run_io(void)
-{
-	struct peer *listen_server;
-	int listen_fd;
-
-	if ((listen_server = setup_listen_socket()) == NULL)  {
-		return -1;
-	}
-	listen_fd = listen_server->fd;
-
-	while (1) {
-		int peer_fd;
-		if (unlikely(shall_close)) {
-			break;
-		}
-		peer_fd = accept(listen_fd, NULL, NULL);
-		if (peer_fd == -1) {
-			if (errno == EINTR) {
-				continue;
-			} else {
-				goto accept_failed;
-			}
-		} else {
-			struct peer *peer;
-			pthread_t thread_id;
-			pthread_attr_t attr;
-			static const int tcp_nodelay_on = 1;
-
-			if (unlikely(setsockopt(peer_fd, IPPROTO_TCP, TCP_NODELAY, &tcp_nodelay_on, sizeof(tcp_nodelay_on)) < 0)) {
-				goto no_delay_failed;
-			}
-
-			peer = alloc_peer(peer_fd);
-			if (unlikely(peer == NULL)) {
-				fprintf(stderr, "Could not allocate peer!\n");
-				goto alloc_peer_failed;
-			}
-
-			if (pthread_attr_init(&attr) != 0) {
-				fprintf(stderr, "could set init thread attribute!\n");
-				goto pthread_attr_init_failed;
-			}
-			if (pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_DETACHED) != 0) {
-				fprintf(stderr, "could set detach attribute for peer thread!\n");
-				goto pthread_setdetach_failed;
-			}
-			if (pthread_create(&thread_id, NULL, handle_client, peer) != 0) {
-				fprintf(stderr, "could not create thread for peer!\n");
-				goto pthread_create_failed;
-			}
-			continue;
-
-		pthread_create_failed:
-		pthread_setdetach_failed:
-		pthread_attr_init_failed:
-		alloc_peer_failed:
-		no_delay_failed:
-			close(peer_fd);
-		accept_failed:
-			close_peer_connection(listen_server, listen_server->fd);
-			return -1;
-		}
-	}
-	close_peer_connection(listen_server, listen_server->fd);
-	return 0;
 }
 
 int main()
@@ -166,7 +25,7 @@ int main()
 		goto create_setter_hashtable_failed;
 	}
 
-	if (run_io() < 0) {
+	if (run_io(&shall_close) < 0) {
 		goto run_io_failed;
 	}
 
