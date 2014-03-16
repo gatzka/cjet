3187084cdd29f124dbf25c9a4cbab50937011d7b
Stephan Gatzka <stephan.gatzka@gmail.com>
2014-03-16 22:17:52 +0100

    Use alpha to make mean line more visible.

--- test/io_gplot.R
+++ test/io_gplot.R
@@ -11,6 +11,9 @@ colnames(m) <- c("MSGSIZE", "multithread", "epoll", "diff")
 
 df = melt(m, id.vars ="MSGSIZE", measure.vars = c("multithread","epoll"), variable.name="IO", value.name="time")
 
-ggplot(data=df, aes(MSGSIZE, time, colour=IO)) + geom_point() + geom_smooth()
+g <- ggplot()
+g <- g + geom_point(data=df, aes(x=MSGSIZE, y=time, colour=IO), alpha=0.3)
+g <- g + geom_smooth(data=df, method=loess, aes(x=MSGSIZE, y=time, colour=IO))
+g
 
 ggplot(data=m, aes(x=MSGSIZE, y=diff)) + geom_point() + geom_smooth()
