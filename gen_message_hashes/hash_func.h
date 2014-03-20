966f3b337731501c32d2f6afc5fe5feb0bf668bd
Stephan Gatzka <stephan.gatzka@gmail.com>
2014-03-20 09:53:27 +0100

    Remove program to generate message hashes.

--- gen_message_hashes/hash_func.h
+++
@@ -1,9 +0,0 @@
-static inline uint32_t hash_func_string(const char* key)
-{
-	uint32_t hash = 0;
-	uint32_t c;
-	while ((c = (uint32_t)*key++) != 0)
-		hash = c + (hash << 6u) + (hash << 16u) - hash;
-	return hash;
-}
-
