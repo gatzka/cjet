d959237cf43ea28698e537bd5b49c882bb9cba75
Stephan Gatzka <stephan.gatzka@gmail.com>
2014-03-20 18:28:45 +0100

    Add missing configuration file.

---
+++ linux/mt/config.h
@@ -0,0 +1,19 @@
+#ifndef CJET_LINUX_CONFIG_H
+#define CJET_LINUX_CONFIG_H
+
+#define SERVER_PORT	11122
+#define LISTEN_BACKLOG 40
+/*
+ * It is somehow beneficial if this size is 32 bit aligned.
+ */
+#define MAX_MESSAGE_SIZE 512
+#define WRITE_BUFFER_CHUNK 512
+#define MAX_WRITE_BUFFER_SIZE (10 * (WRITE_BUFFER_CHUNK))
+
+/*
+ * This config parameter the maximum amount of states that can be
+ * handled in a jet. The number of states is 2^STATE_SET_TABLE_ORDER.
+ */
+#define STATE_SET_TABLE_ORDER 13
+
+#endif
