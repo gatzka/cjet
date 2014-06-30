41e299d84b4d09fbf180078ed0558e46b718e563
Stephan Gatzka <stephan.gatzka@gmail.com>
2014-06-30 21:40:56 +0200

    Specifiy archiver and ranlib to support link time optimization.

--- linux/analyzer.cmake
+++ linux/analyzer.cmake
@@ -1,6 +1,8 @@
 # this one is important
 SET(CMAKE_SYSTEM_NAME Linux)
 
-SET(CMAKE_C_COMPILER "/usr/lib/clang-analyzer/scan-build/ccc-analyzer")
-SET(CMAKE_CXX_COMPILER "/usr/lib/clang-analyzer/scan-build/c++-analyzer")
+SET(CMAKE_C_COMPILER /usr/lib/clang-analyzer/scan-build/ccc-analyzer)
+SET(CMAKE_CXX_COMPILER /usr/lib/clang-analyzer/scan-build/c++-analyzer)
+SET(CMAKE_AR gcc-ar)
+SET(CMAKE_RANLIB gcc-ranlib)
 
