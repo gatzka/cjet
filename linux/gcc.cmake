e125a7022c53f1fbd5782b41008dfd47ad3fc08c
Stephan Gatzka <stephan.gatzka@gmail.com>
2014-06-30 21:31:30 +0200

    Use gcc-ar and gcc-ranlib to support link time optimization with gcc >= 4.9.

--- linux/gcc.cmake
+++ linux/gcc.cmake
@@ -1,6 +1,8 @@
 # this one is important
 SET(CMAKE_SYSTEM_NAME Linux)
 
-SET(CMAKE_C_COMPILER "gcc")
-SET(CMAKE_CXX_COMPILER "g++")
+SET(CMAKE_C_COMPILER gcc)
+SET(CMAKE_CXX_COMPILER g++)
+SET(CMAKE_AR gcc-ar)
+SET(CMAKE_RANLIB gcc-ranlib)
 
