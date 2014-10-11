b620e362739562fe2128a90bde5714d7fe96c31f
Stephan Gatzka <stephan.gatzka@gmail.com>
2014-10-11 18:28:53 +0200

    Minimize toolchain files for clang.

--- linux/analyzer.cmake
+++ linux/analyzer.cmake
@@ -3,6 +3,4 @@ SET(CMAKE_SYSTEM_NAME Linux)
 
 SET(CMAKE_C_COMPILER /usr/lib/clang-analyzer/scan-build/ccc-analyzer)
 SET(CMAKE_CXX_COMPILER /usr/lib/clang-analyzer/scan-build/c++-analyzer)
-SET(CMAKE_AR gcc-ar)
-SET(CMAKE_RANLIB gcc-ranlib)
 
