b797f409f776c1a2c8632fbc646b77aa4be11d84
Stephan Gatzka <stephan.gatzka@gmail.com>
2014-10-15 07:06:21 +0200

    Remove no longer required parameter CONFIG_WRITE_BUFFER_CHUNK.

--- linux/mt/config.h
+++ linux/mt/config.h
@@ -10,8 +10,7 @@ enum {CONFIG_CHECK_JSON_LENGTH = 0};
  * It is somehow beneficial if this size is 32 bit aligned.
  */
 enum {CONFIG_MAX_MESSAGE_SIZE = 512};
-enum {CONFIG_WRITE_BUFFER_CHUNK = 512};
-enum {CONFIG_MAX_WRITE_BUFFER_SIZE = 10 * CONFIG_WRITE_BUFFER_CHUNK};
+enum {CONFIG_MAX_WRITE_BUFFER_SIZE = 5120};
 
 /*
  * This config parameter the maximum amount of states that can be
