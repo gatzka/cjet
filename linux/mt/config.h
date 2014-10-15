a5aca10b61c1343728428f73fe904152fc8b3972
Stephan Gatzka <stephan.gatzka@gmail.com>
2014-10-15 06:31:38 +0200

    Switch config parameters from defines to enums.

--- linux/mt/config.h
+++ linux/mt/config.h
@@ -1,21 +1,23 @@
 #ifndef CJET_LINUX_CONFIG_H
 #define CJET_LINUX_CONFIG_H
 
-#define SERVER_PORT	11122
-#define LISTEN_BACKLOG 40
+enum {SERVER_PORT = 11122};
+enum {LISTEN_BACKLOG = 40};
+
+enum {CONFIG_CHECK_JSON_LENGTH = 0};
+
 /*
  * It is somehow beneficial if this size is 32 bit aligned.
  */
-#define MAX_MESSAGE_SIZE 512
-#define WRITE_BUFFER_CHUNK 512
-#define MAX_WRITE_BUFFER_SIZE (10 * (WRITE_BUFFER_CHUNK))
+enum {MAX_MESSAGE_SIZE = 512};
+enum {WRITE_BUFFER_CHUNK = 512};
+enum {MAX_WRITE_BUFFER_SIZE = 10 * WRITE_BUFFER_CHUNK};
 
 /*
  * This config parameter the maximum amount of states that can be
  * handled in a jet. The number of states is 2^STATE_TABLE_ORDER.
  */
-#define STATE_TABLE_ORDER 13
-
-#define MAX_NUMBER_OF_PEERS 10
+enum {STATE_TABLE_ORDER = 13};
+enum {MAX_NUMBER_OF_PEERS = 10};
 
 #endif
