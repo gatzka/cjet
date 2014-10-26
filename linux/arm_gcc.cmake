f3d014a689e2d2af09b7b639acc9f2f104506914
Stephan Gatzka <stephan.gatzka@gmail.com>
2014-10-26 17:34:50 +0100

    Remove absolute path to arm toolchain.

--- linux/arm_gcc.cmake
+++ linux/arm_gcc.cmake
@@ -1,6 +1,6 @@
 # this one is important
 SET(CMAKE_SYSTEM_NAME Linux)
 
-SET(CMAKE_C_COMPILER "/opt/OSELAS.Toolchain-2012.12.1/arm-cortexa9-linux-gnueabihf/gcc-4.7.3-glibc-2.16.0-binutils-2.22-kernel-3.6-sanitized/bin/arm-cortexa9-linux-gnueabihf-gcc")
-SET(CMAKE_CXX_COMPILER "/opt/OSELAS.Toolchain-2012.12.1/arm-cortexa9-linux-gnueabihf/gcc-4.7.3-glibc-2.16.0-binutils-2.22-kernel-3.6-sanitized/bin/arm-cortexa9-linux-gnueabihf-g++")
+SET(CMAKE_C_COMPILER arm-linux-gnueabihf-gcc)
+SET(CMAKE_CXX_COMPILER arm-linux-gnueabihf-g++)
 
