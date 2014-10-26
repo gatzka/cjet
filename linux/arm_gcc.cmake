581112bf4365ffb208e362dd1185882d48a56062
Stephan Gatzka <stephan.gatzka@gmail.com>
2014-10-26 18:45:27 +0100

    Set cross compile environment for cmake.

--- linux/arm_gcc.cmake
+++ linux/arm_gcc.cmake
@@ -1,7 +1,23 @@
 # this one is important
 SET(CMAKE_SYSTEM_NAME Linux)
+SET(CMAKE_SYSTEM_PROCESSOR arm)
+
+SET(CJET_TOOLCHAIN_PATH /opt/linaro/gcc-linaro-arm-linux-gnueabihf-4.9-2014.09_linux/)
 
 SET(triple arm-linux-gnueabihf)
-SET(CMAKE_C_COMPILER ${triple}-gcc)
-SET(CMAKE_CXX_COMPILER ${triple}-g++)
+
+SET(CMAKE_C_COMPILER   ${CJET_TOOLCHAIN_PATH}/bin/${triple}-gcc)
+SET(CMAKE_CXX_COMPILER ${CJET_TOOLCHAIN_PATH}/bin/${triple}-g++)
+SET(CMAKE_AR           ${CJET_TOOLCHAIN_PATH}/bin/${triple}-ar)
+SET(CMAKE_LINKER       ${CJET_TOOLCHAIN_PATH}/bin/${triple}-ld)
+SET(CMAKE_NM           ${CJET_TOOLCHAIN_PATH}/bin/${triple}-nm)
+SET(CMAKE_OBJCOPY      ${CJET_TOOLCHAIN_PATH}/bin/${triple}-objcopy)
+SET(CMAKE_OBJDUMP      ${CJET_TOOLCHAIN_PATH}/bin/${triple}-objdump)
+SET(CMAKE_RANLIB       ${CJET_TOOLCHAIN_PATH}/bin/${triple}-ranlib)
+SET(CMAKE_STRIP        ${CJET_TOOLCHAIN_PATH}/bin/${triple}-strip)
+
+SET(CMAKE_FIND_ROOT_PATH ${CJET_TOOLCHAIN_PATH}/arm-linux-gnueabihf/libc/)
+SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+SET(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+SET(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 
--- linux/ppc_gcc.cmake
+++ linux/ppc_gcc.cmake
@@ -1,7 +1,24 @@
 # this one is important
 SET(CMAKE_SYSTEM_NAME Linux)
+SET(CMAKE_SYSTEM_PROCESSOR powerpc)
+
+SET(CJET_TOOLCHAIN_PATH /opt/poky/1.6/sysroots/)
 
 SET(triple powerpc-poky-linux)
-SET(CMAKE_C_COMPILER ${triple}-gcc)
-SET(CMAKE_CXX_COMPILER ${triple}-g++)
+
+SET(CMAKE_C_COMPILER   ${CJET_TOOLCHAIN_PATH}/x86_64-pokysdk-linux/usr/bin/${triple}/${triple}-gcc)
+SET(CMAKE_CXX_COMPILER ${CJET_TOOLCHAIN_PATH}/x86_64-pokysdk-linux/usr/bin/${triple}/${triple}-g++)
+SET(CMAKE_AR           ${CJET_TOOLCHAIN_PATH}/x86_64-pokysdk-linux/usr/bin/${triple}/${triple}-ar)
+SET(CMAKE_LINKER       ${CJET_TOOLCHAIN_PATH}/x86_64-pokysdk-linux/usr/bin/${triple}/${triple}-ld)
+SET(CMAKE_NM           ${CJET_TOOLCHAIN_PATH}/x86_64-pokysdk-linux/usr/bin/${triple}/${triple}-nm)
+SET(CMAKE_OBJCOPY      ${CJET_TOOLCHAIN_PATH}/x86_64-pokysdk-linux/usr/bin/${triple}/${triple}-objcopy)
+SET(CMAKE_OBJDUMP      ${CJET_TOOLCHAIN_PATH}/x86_64-pokysdk-linux/usr/bin/${triple}/${triple}-objdump)
+SET(CMAKE_RANLIB       ${CJET_TOOLCHAIN_PATH}/x86_64-pokysdk-linux/usr/bin/${triple}/${triple}-ranlib)
+SET(CMAKE_STRIP        ${CJET_TOOLCHAIN_PATH}/x86_64-pokysdk-linux/usr/bin/${triple}/${triple}-strip)
+
+SET(CMAKE_FIND_ROOT_PATH ${CJET_TOOLCHAIN_PATH}/ppc7400-poky-linux/)
+SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+SET(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+SET(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+
 
