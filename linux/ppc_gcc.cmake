f5fceaaf7a6d708234ca0ec1537733333cf05e81
Stephan Gatzka <stephan.gatzka@gmail.com>
2014-10-26 08:57:58 +0100

    Take out absolute path to PowerPC compiler.

--- linux/ppc_gcc.cmake
+++ linux/ppc_gcc.cmake
@@ -1,6 +1,6 @@
 # this one is important
 SET(CMAKE_SYSTEM_NAME Linux)
 
-SET(CMAKE_C_COMPILER "/opt/poky/1.5.1/sysroots/x86_64-pokysdk-linux/usr/bin/powerpc-poky-linux/powerpc-poky-linux-gcc")
-SET(CMAKE_CXX_COMPILER "/opt/poky/1.5.1/sysroots/x86_64-pokysdk-linux/usr/bin/powerpc-poky-linux/powerpc-poky-linux-g++")
+SET(CMAKE_C_COMPILER powerpc-poky-linux-gcc)
+SET(CMAKE_CXX_COMPILER powerpc-poky-linux-g++)
 
