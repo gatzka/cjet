d75e48e83e9d187a20c410ee58ab55b471800e43
Stephan Gatzka <stephan.gatzka@gmail.com>
2014-11-26 17:48:10 +0100

    Avoid assignment in sub-expressions.

--- hashtable.h
+++ hashtable.h
@@ -255,9 +255,11 @@ struct hashtable_string { \
 static inline u32 hash_func_##name##_string(const char* key) \
 { \
 	u32 hash = 0; \
-	u32 c; \
-	while ((c = (u32)*key++) != 0) \
+	u32 c = (u32)*key++; \
+	while (c != 0) { \
 		hash = c + (hash << 6u) + (hash << 16u) - hash; \
+		c = (u32)*key++; \
+	} \
 	hash = (hash * (hash32_magic)) >> (32u - (order)); \
 	return hash; \
 } \
