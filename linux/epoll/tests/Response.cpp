239c6f3adf3a74608a4314dceaaef9531d90d7cc
Stephan Gatzka <stephan.gatzka@gmail.com>
2014-12-10 17:32:52 +0100

    Add test for creating remaining error objects.

--- linux/epoll/tests/Response.cpp
+++ linux/epoll/tests/Response.cpp
@@ -66,3 +66,85 @@ BOOST_AUTO_TEST_CASE(internal_error_response)
 	cJSON_Delete(id);
 	cJSON_Delete(response);
 }
+
+BOOST_AUTO_TEST_CASE(invalid_request_response)
+{
+	const char *tag = "reason";
+	const char *reason = "neither request nor response";
+	cJSON *id = cJSON_CreateString("request1");
+	cJSON *error = create_invalid_request_error(tag, reason);
+	cJSON *response = create_error_response(id, error);
+
+	cJSON *err = cJSON_GetObjectItem(response, "error");
+	BOOST_CHECK(err->type == cJSON_Object);
+
+	cJSON *code = cJSON_GetObjectItem(err, "code");
+	BOOST_CHECK(code->type == cJSON_Number && code->valueint == -32600);
+
+	cJSON *message = cJSON_GetObjectItem(err, "message");
+	BOOST_CHECK(message->type == cJSON_String && strcmp(message->valuestring, "Invalid Request") == 0);
+	cJSON *data = cJSON_GetObjectItem(err, "data");
+	BOOST_CHECK(data != NULL && data->type == cJSON_Object);
+
+	cJSON *r = cJSON_GetObjectItem(data, tag);
+	BOOST_CHECK(r->type == cJSON_String && strcmp(r->valuestring, reason) == 0);
+
+
+	cJSON_Delete(id);
+	cJSON_Delete(response);
+}
+
+BOOST_AUTO_TEST_CASE(method_not_found_response)
+{
+	const char *tag = "reason";
+	const char *reason = "calling";
+	cJSON *id = cJSON_CreateString("request1");
+	cJSON *error = create_method_not_found_error(tag, reason);
+	cJSON *response = create_error_response(id, error);
+
+	cJSON *err = cJSON_GetObjectItem(response, "error");
+	BOOST_CHECK(err->type == cJSON_Object);
+
+	cJSON *code = cJSON_GetObjectItem(err, "code");
+	BOOST_CHECK(code->type == cJSON_Number && code->valueint == -32601);
+
+	cJSON *message = cJSON_GetObjectItem(err, "message");
+	BOOST_CHECK(message->type == cJSON_String && strcmp(message->valuestring, "Method not found") == 0);
+	cJSON *data = cJSON_GetObjectItem(err, "data");
+	BOOST_CHECK(data != NULL && data->type == cJSON_Object);
+
+	cJSON *r = cJSON_GetObjectItem(data, tag);
+	BOOST_CHECK(r->type == cJSON_String && strcmp(r->valuestring, reason) == 0);
+
+
+	cJSON_Delete(id);
+	cJSON_Delete(response);
+}
+
+BOOST_AUTO_TEST_CASE(invalid_params_response)
+{
+	const char *tag = "not exists";
+	const char *reason = "/foo/bar/";
+	cJSON *id = cJSON_CreateString("request1");
+	cJSON *error = create_invalid_params_error(tag, reason);
+	cJSON *response = create_error_response(id, error);
+
+	cJSON *err = cJSON_GetObjectItem(response, "error");
+	BOOST_CHECK(err->type == cJSON_Object);
+
+	cJSON *code = cJSON_GetObjectItem(err, "code");
+	BOOST_CHECK(code->type == cJSON_Number && code->valueint == -32602);
+
+	cJSON *message = cJSON_GetObjectItem(err, "message");
+	BOOST_CHECK(message->type == cJSON_String && strcmp(message->valuestring, "Invalid params") == 0);
+	cJSON *data = cJSON_GetObjectItem(err, "data");
+	BOOST_CHECK(data != NULL && data->type == cJSON_Object);
+
+	cJSON *r = cJSON_GetObjectItem(data, tag);
+	BOOST_CHECK(r->type == cJSON_String && strcmp(r->valuestring, reason) == 0);
+
+
+	cJSON_Delete(id);
+	cJSON_Delete(response);
+}
+
