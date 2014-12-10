99fd156093c7ac955d1842cdb28765707e8e31fb
Stephan Gatzka <stephan.gatzka@gmail.com>
2014-12-10 16:44:44 +0100

    Exclude tests from coverage report.

--- linux/epoll/tests/make_coverage.sh
+++ linux/epoll/tests/make_coverage.sh
@@ -2,6 +2,6 @@
 lcov --base-directory . --directory . --zerocounters -q
 ctest
 lcov --quiet --directory . --capture --output-file app.info
-lcov --quiet --remove app.info "/usr*" "/opt/boost/*" "*/cjet/json/*" -o app.info.filter
+lcov --quiet --remove app.info "/usr*" "/opt/boost/*" "*/cjet/json/*" "*/cjet/linux/epoll/tests/*" -o app.info.filter
 genhtml --quiet app.info.filter -o report  >/dev/null 2>&1
 xdg-open report/index.html
