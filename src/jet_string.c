7f5e4d6c6b402ade2777408d83a9e0f7983d1311
Stephan Gatzka <stephan.gatzka@gmail.com>
2015-06-02 19:41:07 +0200

    Put string functions into its own file.

--- src/jet_string.c
+++ src/jet_string.c
@@ -24,10 +24,34 @@
  * SOFTWARE.
  */
 
-#ifndef CJET_STRING_H
-#define CJET_STRING_H
+#include <stdlib.h>
+#include <string.h>
 
-char *duplicate_string(const char *s);
-const char *jet_strcasestr(const char *haystack, const char *needle);
+#include "compiler.h"
+#include "jet_string.h"
 
-#endif
+char *duplicate_string(const char *s)
+{
+	size_t length = strlen(s);
+	char *new_string = malloc(length + 1);
+	if (likely(new_string != NULL)) {
+		strncpy(new_string, s, length + 1);
+	}
+	return new_string;
+}
+
+const char *jet_strcasestr(const char *haystack, const char *needle)
+{
+	size_t hay_len = strlen(haystack);
+	size_t needle_len = strlen(needle);
+	while (hay_len >= needle_len) {
+		if (strncasecmp(haystack, needle, needle_len) == 0)
+		    return haystack;
+
+		haystack++;
+		hay_len--;
+	}
+
+	return NULL;
+}
+
