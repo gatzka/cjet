90bd79b8f4b7143c33f1d7df1c52575677e96fac
Stephan Gatzka <stephan.gatzka@hbm.com>
2016-06-23 08:41:13 +0200

    Make this header useable with C++.

--- src/jet_endian.h
+++ src/jet_endian.h
@@ -29,11 +29,20 @@
 
 #include <stdint.h>
 
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 uint16_t jet_be16toh(uint16_t big_endian_16bits);
 uint32_t jet_be32toh(uint32_t big_endian_32bits);
 uint64_t jet_be64toh(uint64_t big_endian_64bits);
 
 uint16_t jet_htobe16(uint16_t host_endian_16bits);
 uint64_t jet_htobe64(uint64_t host_endian_16bits);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
