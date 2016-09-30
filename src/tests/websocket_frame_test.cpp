4070ae42da324b8f2d13b8a4fa2fcea31436c1cf
Stephan Gatzka <stephan.gatzka@hbm.com>
2016-09-30 11:26:57 +0200

    Test sending zero-length text messages and sending from a client.

--- src/tests/websocket_frame_test.cpp
+++ src/tests/websocket_frame_test.cpp
@@ -576,11 +576,18 @@ BOOST_AUTO_TEST_CASE(test_receive_large_binary_message)
 
 BOOST_AUTO_TEST_CASE(test_sending_text_message)
 {
-	bool is_server = true;
-	F f(is_server, 5000);
+	const char *messages[2] = {"Hello World!", ""};
+	bool is_server[2] = {true, false};
 
-	char message[] = "Hello World!";
-	websocket_send_text_frame(&f.ws, message, ::strlen(message));
-	websocket_close(&f.ws, WS_CLOSE_GOING_AWAY);
-	BOOST_CHECK_MESSAGE(check_frame(WS_OPCODE_TEXT, message, ::strlen(message)), "Not a valid text message sent!");
+	for (unsigned int j = 0; j < ARRAY_SIZE(is_server); j++) {
+		for (unsigned int i = 0; i < ARRAY_SIZE(messages); i++) {
+			F f(is_server[j], 5000);
+			char *message = (char *)::malloc(::strlen(messages[i]) + 1);
+			::strcpy(message, messages[i]);
+			websocket_send_text_frame(&f.ws, message, ::strlen(message));
+			websocket_close(&f.ws, WS_CLOSE_GOING_AWAY);
+			BOOST_CHECK_MESSAGE(check_frame(WS_OPCODE_TEXT, messages[i], ::strlen(messages[i])), "Not a valid text message sent!");
+			free(message);
+		}
+	}
 }
