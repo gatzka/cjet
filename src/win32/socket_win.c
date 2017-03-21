ec108eb7dfc27271fe95608a8b8804ff573cb6e0
Stephan Gatzka <stephan.gatzka@gmail.com>
2017-03-21 20:04:17 +0100

    Add stub for windows socket implementation.

---
+++ src/win32/socket_win.c
@@ -0,0 +1,55 @@
+/*
+*The MIT License (MIT)
+*
+* Copyright (c) <2017> <Stephan Gatzka>
+*
+* Permission is hereby granted, free of charge, to any person obtaining
+* a copy of this software and associated documentation files (the
+* "Software"), to deal in the Software without restriction, including
+* without limitation the rights to use, copy, modify, merge, publish,
+* distribute, sublicense, and/or sell copies of the Software, and to
+* permit persons to whom the Software is furnished to do so, subject to
+* the following conditions:
+*
+* The above copyright notice and this permission notice shall be
+* included in all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+* NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+* BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+* ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+* CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*/
+
+#include <stddef.h>
+
+#include "compiler.h"
+#include "socket.h"
+
+cjet_ssize_t socket_read(socket_type sock, void *buf, size_t count)
+{
+	return 0;
+}
+
+cjet_ssize_t socket_writev_with_prefix(socket_type sock, void *buf, size_t len, struct socket_io_vector *io_vec, unsigned int count)
+{
+	return 0;
+}
+
+int socket_close(socket_type sock)
+{
+	return 0;
+}
+
+enum cjet_system_error get_socket_error(void)
+{
+	return 0;
+}
+
+const char *get_socket_error_msg(enum cjet_system_error err)
+{
+	return "Hello";
+}
