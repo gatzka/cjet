5471bc6fb0f80034eddc419926c90a7d5bb7a358
Felix Retsch <felix.retsch@hbm.com>
2017-06-02 09:17:51 +0200

    Code style correction utf8_checker_test
    add prefix cjet to utf8_checker and changed return type to bool

--- src/tests/utf8_checker_test.cpp
+++ src/tests/utf8_checker_test.cpp
@@ -1,5 +1,5 @@
 /*
- *The MIT License (MIT)
+ * The MIT License (MIT)
  *
  * Copyright (c) <2017> <Felix Retsch>
  *
@@ -33,7 +33,7 @@
 #include "utf8_checker.h"
 
 static const int invalid_message_size = 10;
-static uint8_t invalid_message[invalid_message_size][4] = {{0xC0,0x00,0x00,0x00},	//invalid start
+static const uint8_t invalid_message[invalid_message_size][4] = {{0xC0,0x00,0x00,0x00},	//invalid start
 														   {0xF6,0x00,0x00,0x00},	//invalid start
 														   {0x80,0x00,0x00,0x00},	//invalid start
 														   {0xC2,0x80,0x80,0x00},	//invalid continuation
@@ -44,48 +44,44 @@ static uint8_t invalid_message[invalid_message_size][4] = {{0xC0,0x00,0x00,0x00}
 														   {0xF0,0x8F,0x80,0x80},	//reserved zone
 														   {0xF4,0x90,0x80,0x80}};	//reserved zone
 
-static char valid_message[] = "Hello-µ@ßöäüàá-UTF-8!!";
-static uint8_t valid_message_long[] = {0xF1,0x80,0x80,0x80,0xF2,0xA0,0xA0,0xA0};
+static const char valid_message[] = "Hello-µ@ßöäüàá-UTF-8!!";
+static const uint8_t valid_message_long[] = {0xF1,0x80,0x80,0x80,0xF2,0xA0,0xA0,0xA0};
 
 struct F {
 	F ()
 	{
-		init_checker(&c);
+		cjet_init_checker(&c);
 	}
 
 	~F()
 	{
 	}
 
-	struct utf8_checker c;
+	struct cjet_utf8_checker c;
 };
 
 BOOST_AUTO_TEST_CASE(test_message_zero_length)
 {
 	F f;
 	char zero = '\0';
-	int ret = is_text_valid(&f.c, &zero, 0);
-	BOOST_CHECK_MESSAGE(ret == 1, "Message should be valid!");
-	BOOST_CHECK_MESSAGE(ret >= 0, "Internal error occured!");
+	int ret = cjet_is_text_valid(&f.c, &zero, 0);
+	BOOST_CHECK_MESSAGE(ret == true, "Message should be valid!");
 }
 
 BOOST_AUTO_TEST_CASE(test_valid_message)
 {
 	F f;
-	int ret = is_text_valid(&f.c, valid_message, sizeof(valid_message));
-	BOOST_CHECK_MESSAGE(ret == 1, "Message should be valid!");
-	BOOST_CHECK_MESSAGE(ret >= 0, "Internal error occured!");
+	int ret = cjet_is_text_valid(&f.c, valid_message, sizeof(valid_message));
+	BOOST_CHECK_MESSAGE(ret == true, "Message should be valid!");
 }
 
 BOOST_AUTO_TEST_CASE(test_valid_message_fragmented_on_codepoints)
 {
 	F f;
-	int ret = is_text_valid(&f.c, valid_message, 15);
-	BOOST_CHECK_MESSAGE(ret == 1, "Message should be valid!");
-	BOOST_CHECK_MESSAGE(ret >= 0, "Internal error occured!");
-	ret = is_text_valid(&f.c, valid_message + 15, sizeof(valid_message) - 15);
-	BOOST_CHECK_MESSAGE(ret == 1, "Message should be valid!");
-	BOOST_CHECK_MESSAGE(ret >= 0, "Internal error occured!");
+	int ret = cjet_is_text_valid(&f.c, valid_message, 15);
+	BOOST_CHECK_MESSAGE(ret == true, "Message should be valid!");
+	ret = cjet_is_text_valid(&f.c, valid_message + 15, sizeof(valid_message) - 15);
+	BOOST_CHECK_MESSAGE(ret == true, "Message should be valid!");
 }
 
 BOOST_AUTO_TEST_CASE(test_valid_message_fragmented_between_letters)
@@ -93,29 +89,25 @@ BOOST_AUTO_TEST_CASE(test_valid_message_fragmented_between_letters)
 	F f;
 	int ret;
 	for (uint i = 0; i < sizeof(valid_message); i++) {
-		ret = is_text_valid(&f.c, valid_message + i, 1);
-		BOOST_CHECK_MESSAGE(ret == 1, "Message should be valid!");
-		BOOST_CHECK_MESSAGE(ret >= 0, "Internal error occured!");
+		ret = cjet_is_text_valid(&f.c, valid_message + i, 1);
+		BOOST_CHECK_MESSAGE(ret == true, "Message should be valid!");
 	}
 }
 
 BOOST_AUTO_TEST_CASE(test_valid_message_long)
 {
 	F f;
-	int ret = is_byte_sequence_valid(&f.c, valid_message_long, sizeof(valid_message_long));
-	BOOST_CHECK_MESSAGE(ret == 1, "Message should be valid!");
-	BOOST_CHECK_MESSAGE(ret >= 0, "Internal error occured!");
+	int ret = cjet_is_byte_sequence_valid(&f.c, valid_message_long, sizeof(valid_message_long));
+	BOOST_CHECK_MESSAGE(ret == true, "Message should be valid!");
 }
 
 BOOST_AUTO_TEST_CASE(test_valid_message_fragmented_on_codepoints_long)
 {
 	F f;
-	int ret = is_byte_sequence_valid(&f.c, valid_message_long, 4);
-	BOOST_CHECK_MESSAGE(ret == 1, "Message should be valid!");
-	BOOST_CHECK_MESSAGE(ret >= 0, "Internal error occured!");
-	ret = is_byte_sequence_valid(&f.c, valid_message_long + 4, sizeof(valid_message_long) - 4);
-	BOOST_CHECK_MESSAGE(ret == 1, "Message should be valid!");
-	BOOST_CHECK_MESSAGE(ret >= 0, "Internal error occured!");
+	int ret = cjet_is_byte_sequence_valid(&f.c, valid_message_long, 4);
+	BOOST_CHECK_MESSAGE(ret == true, "Message should be valid!");
+	ret = cjet_is_byte_sequence_valid(&f.c, valid_message_long + 4, sizeof(valid_message_long) - 4);
+	BOOST_CHECK_MESSAGE(ret == true, "Message should be valid!");
 }
 
 BOOST_AUTO_TEST_CASE(test_valid_message_fragmented_between_letters_long)
@@ -123,9 +115,8 @@ BOOST_AUTO_TEST_CASE(test_valid_message_fragmented_between_letters_long)
 	F f;
 	int ret;
 	for (uint i = 0; i < sizeof(valid_message_long); i++) {
-		ret = is_byte_sequence_valid(&f.c, valid_message_long + i, 1);
-		BOOST_CHECK_MESSAGE(ret == 1, "Message should be valid!");
-		BOOST_CHECK_MESSAGE(ret >= 0, "Internal error occured!");
+		ret = cjet_is_byte_sequence_valid(&f.c, valid_message_long + i, 1);
+		BOOST_CHECK_MESSAGE(ret == true, "Message should be valid!");
 	}
 }
 
@@ -134,21 +125,18 @@ BOOST_AUTO_TEST_CASE(test_invalid_message)
 	int ret;
 	for (int i = 0; i < invalid_message_size; i++) {
 		F f;
-		ret = is_byte_sequence_valid(&f.c, invalid_message[i], sizeof(invalid_message[i]));
-		BOOST_CHECK_MESSAGE(ret == 0, "Message should be invalid!");
-		BOOST_CHECK_MESSAGE(ret >= 0, "Internal error occured!");
+		ret = cjet_is_byte_sequence_valid(&f.c, invalid_message[i], sizeof(invalid_message[i]));
+		BOOST_CHECK_MESSAGE(ret == false, "Message should be invalid!");
 	}
 }
 
 BOOST_AUTO_TEST_CASE(test_invalid_message_fragmented_on_codepoints)
 {
 	F f;
-	int ret = is_byte_sequence_valid(&f.c, valid_message_long, sizeof(valid_message_long));
+	int ret = cjet_is_byte_sequence_valid(&f.c, valid_message_long, sizeof(valid_message_long));
 	BOOST_CHECK_MESSAGE(ret == 1, "Message should be valid!");
-	BOOST_CHECK_MESSAGE(ret >= 0, "Internal error occured!");
-	ret = is_byte_sequence_valid(&f.c, invalid_message[1], sizeof(invalid_message[1]));
-	BOOST_CHECK_MESSAGE(ret == 0, "Message should be invalid!");
-	BOOST_CHECK_MESSAGE(ret >= 0, "Internal error occured!");
+	ret = cjet_is_byte_sequence_valid(&f.c, invalid_message[1], sizeof(invalid_message[1]));
+	BOOST_CHECK_MESSAGE(ret == false, "Message should be invalid!");
 }
 
 BOOST_AUTO_TEST_CASE(test_invalid_message_fragmented_between_letters)
@@ -157,10 +145,9 @@ BOOST_AUTO_TEST_CASE(test_invalid_message_fragmented_between_letters)
 	for (int i = 0; i < invalid_message_size; i++) {
 		F f;
 		for (uint j = 0; j < sizeof(invalid_message[i]); j++) {
-			ret = is_byte_sequence_valid(&f.c, &invalid_message[i][j], 1);
+			ret = cjet_is_byte_sequence_valid(&f.c, &invalid_message[i][j], 1);
 			if (ret < 1) break;
 		}
-		BOOST_CHECK_MESSAGE(ret == 0, "Message should be invalid!");
-		BOOST_CHECK_MESSAGE(ret >= 0, "Internal error occured!");
+		BOOST_CHECK_MESSAGE(ret == false, "Message should be invalid!");
 	}
 }
